f35c02a655998f30c73fb8f6dc11e902f0be2cd8
HYUK SUN KWON <117493461+includekhs@users.noreply.github.com>
2023-10-08 00:22:29 +0900

    Create init_db.py

---
+++ orangepi03/init_db.py
@@ -0,0 +1,28 @@
+import sqlit3
+import subprocess
+import os
+
+db_file = "/app/orangepi03/data/broker.db"
+
+conn = sqlite3.connect(db_file)
+
+cursor = conn.cursor()
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS dht (
+id INTEGER PRIMARY KEY,
+topic TEXT,
+temperature REAL,
+humidity REAL
+);
+"""
+
+cursor.execute(create_table_query)
+
+conn.commit()
+
+conn.close()
+
+print("Database initialization completed.")
+
+subprocess.run(["python3", "mqtt_broker.py"])
